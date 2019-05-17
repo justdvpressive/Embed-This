25fcbab22f470778c73a270888a26a19cfdae500
justdvpressive <46937250+justdvpressive@users.noreply.github.com>
2019-05-18 00:20:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 # CONFIG
 # ---------
-token = "TOKEN_HERE" # This is what the bot uses to log into Discord.
-prefix = "!" # This will be used at the start of commands.
-embed_role = "ROLE_NAME_HERE" # The role in your server used for embedding.
+token = "NTcyMTIzNTU2NzI1NTIyNDcz.XN8zjA.LEyio-Nc4XOaBAQFk_FQXNaDIsU" # This is what the bot uses to log into Discord.
+prefix = "emb/" # This will be used at the start of commands.
+embed_role = "Embed Creator" # The role in your server used for embedding.
 game = "with embeds!" # This will display as the game on Discord.
 # ----------
 
@@ -18,14 +18,14 @@ async def on_ready():
     chalk.blue ("Ready when you are. ;)") 
     chalk.blue ("Name: {}".format(bot.user.name))
     chalk.blue ("ID: {}".format(bot.user.id))
-    await bot.change_presence(game=discord.Game(name=game))
+    await bot.change_presence(activity=discord.Game(name=game))
 
 @bot.command(pass_context=True)
 async def help(ctx):
     embed = discord.Embed(title="Help!", description="Basically, this is how I'm used.", color=0x00a0ea)
     embed.add_field(name="{}embed".format(prefix), value="Creates a quick embed with the users input after the command is called.")
     embed.add_field(name="{}rembed".format(prefix), value="Let's you embed with more user input. After entering your message the bot will ask questions about the color and thumbnail.")
-    embed.set_footer(text="Embed-This!")
+    embed.set_footer(text="Embed Creator")
     await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
@@ -36,7 +36,7 @@ async def rembed(ctx, *, a_sMessage):
     embed_color = discord.Embed(title="🕑 Tick-Tock", description="Would you like to use a **custom color**? If **yes**, state it. If **no** simply say *no*.", color=0xffff00)
     embed_color.set_footer(text="Simply type a color name such as green in plaintext.")
     embed_thumb = discord.Embed(title="🕑 Tick-Tock", description="Would you like to use a **custom thumbnail**? If **yes**, state it. If **no** simply say *no*.", color=0xffff00)
-    embed_thumb.set_footer(text="Simply type an image URL such as https://da532.com/img/avatar.png in plaintext.")
+    embed_thumb.set_footer(text="Simply type an image URL in plaintext.")
     await bot.delete_message(ctx.message)
     ques1 = await bot.say(embed=embed_color)
     ques1
@@ -93,7 +93,7 @@ async def embed(ctx, *, a_sMessage):
     embed = discord.Embed(description=a_sMessage, color=0x00a0ea)
     embed.set_thumbnail(url=ctx.message.author.avatar_url)
     embed.set_author(name=ctx.message.author.name + " says..")
-    embed.set_footer(text="Embed-This!")
+    embed.set_footer(text="Embed Creator")
     await bot.delete_message(ctx.message)
     await bot.say(embed=embed)
     chalk.green(ctx.message.author.name + " has embedded a message in " + ctx.message.server.name)
